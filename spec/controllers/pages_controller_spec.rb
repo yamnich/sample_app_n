7daca60d3f1a5aac4abf3c5f8883e3bc3e4e5ef0
Tatiana Yamnich <yamnich@gmail.com>
2011-12-29 23:12:30 +0200

    Finished layout and routes

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,11 +1,23 @@
 require 'spec_helper'
 
 describe PagesController do
+  render_views
+
+  before(:each) do
+    @base_title1="Ruby on Rails Tutorial Sample App"
+  end
 
   describe "GET 'home'" do
     it "should be successful" do
       get 'home'
       response.should be_success
+    end
+
+    it "should have the right title" do
+      get 'home'
+      response.should have_selector("title",
+                      :content =>
+                          @base_title1 + " | Home")
     end
   end
 
@@ -14,6 +26,13 @@ describe PagesController do
       get 'contact'
       response.should be_success
     end
+
+    it "should have the right title" do
+      get 'contact'
+      response.should have_selector("title",
+                      :content =>
+                          @base_title1 +" | Contact")
+    end
   end
 
   describe "GET 'about'" do
@@ -21,6 +40,28 @@ describe PagesController do
       get 'about'
       response.should be_success
     end
+
+    it "should have the right title" do
+      get 'about'
+      response.should have_selector("title",
+                      :content =>
+                          @base_title1 +" | About")
+    end
   end
+
+  describe "Get 'help'" do
+    it "should be successful" do
+      get 'help'
+      response.should be_success
+    end
+
+    it "should have the right title" do
+      get 'help'
+      response.should have_selector("title",
+                      :content =>
+                          @base_title1 +" | Help")
+    end
+  end
+
 
 end
